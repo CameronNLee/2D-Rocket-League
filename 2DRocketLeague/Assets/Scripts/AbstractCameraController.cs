819f32409736f56bf173d74229deac0b2d5f3383
FridaWang <379788704@qq.com>
2019-06-03 16:54:53 -0700

    modify player's sprite / implement camera controller / small UI changes

---
+++ 2DRocketLeague/Assets/Scripts/AbstractCameraController.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Obscura
+{
+    [RequireComponent(typeof(Camera))]
+    [RequireComponent(typeof(LineRenderer))]
+    public abstract class AbstractCameraController : MonoBehaviour
+    {
+        [SerializeField]
+        protected bool DrawLogic;
+        [SerializeField]
+        protected GameObject Target_one;
+        [SerializeField]
+        protected GameObject Target_two;
+
+    }
+
+}
---
+++ 2DRocketLeague/Assets/Scripts/MainCameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Obscura
+{
+    public class MainCameraController : AbstractCameraController
+    {
+        
+        private Camera ManagedCamera;
+        private LineRenderer CameraLineRenderer;
+
+        private void Awake()
+        {
+            this.ManagedCamera = this.gameObject.GetComponent<Camera>();
+            this.CameraLineRenderer = this.gameObject.GetComponent<LineRenderer>();
+        }
+
+        //Use the LateUpdate message to avoid setting the camera's position before
+        //GameObject locations are finalized.
+        void LateUpdate()
+        {
+            var targetPosition = (this.Target_one.transform.position + this.Target_two.transform.position) / 2;
+            var cameraPosition = this.ManagedCamera.transform.position;
+
+            if (targetPosition != cameraPosition)
+            {
+                cameraPosition = new Vector3(targetPosition.x, targetPosition.y, cameraPosition.z);
+            }
+
+            this.ManagedCamera.transform.position = cameraPosition;
+
+        }
+    }
+}
