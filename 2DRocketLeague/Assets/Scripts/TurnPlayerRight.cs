e7623e007c36c7d5c0534591e815c50fb8b03948
CameronNLee <cameron.lee.cl@gmail.com>
2019-06-02 20:09:52 -0700

    Added basic Left/Right turning for players.

--- 2DRocketLeague/Assets/Scripts/TurnPlayerRight.cs
+++ 2DRocketLeague/Assets/Scripts/TurnPlayerRight.cs
@@ -4,9 +4,19 @@ namespace Player.Command
 {
     public class TurnPlayerRight : ScriptableObject, IPlayerCommand
     {
+        private float TurnSpeed = 180.0f;
+        private float TurnAxisMovement;
         public void Execute(GameObject gameObject)
         {
-            throw new System.NotImplementedException();
+            var rigidBody = gameObject.GetComponent<Rigidbody>();
+            if (rigidBody != null)
+            {
+                TurnAxisMovement = Input.GetAxis("Horizontal");
+                var rotation = -TurnAxisMovement * TurnSpeed * Time.deltaTime;
+                var turnRotation = Quaternion.Euler(0.0f, 0.0f, rotation);
+                rigidBody.MoveRotation(rigidBody.rotation * turnRotation);
+                // gameObject.GetComponent<Sprit
+            }
         }
     }
 }
