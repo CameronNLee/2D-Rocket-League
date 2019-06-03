c970a07e8466b2870f40f224740b836f76f11bd7
CameronNLee <cameron.lee.cl@gmail.com>
2019-06-03 15:21:04 -0700

    Added rough player movement by pressing shift.

--- 2DRocketLeague/Assets/Scripts/AcceleratePlayer.cs
+++ 2DRocketLeague/Assets/Scripts/AcceleratePlayer.cs
@@ -4,9 +4,15 @@ namespace Player.Command
 {
     public class AcceleratePlayer : ScriptableObject, IPlayerCommand
     {
+        private float MovementSpeed = 1.0f;
         public void Execute(GameObject gameObject)
         {
-            throw new System.NotImplementedException();
+            var rigidBody = gameObject.GetComponent<Rigidbody>();
+            if (rigidBody != null)
+            {
+                rigidBody.transform.position += rigidBody.transform.right * MovementSpeed;
+                // gameObject.GetComponent<SpriteRenderer>().flipX = true;
+            }
         }
     }
 }
