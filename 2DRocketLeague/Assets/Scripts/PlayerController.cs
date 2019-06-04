58b58dd7ad118ba3a588463d96a7f4f33da733ad
FridaWang <379788704@qq.com>
2019-06-04 01:11:55 -0700

    the UI elements are able to show the change in energy (using Space to accelerate when move forward)

--- 2DRocketLeague/Assets/Scripts/PlayerController.cs
+++ 2DRocketLeague/Assets/Scripts/PlayerController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using Player.Command;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -9,6 +10,9 @@ public class PlayerController : MonoBehaviour
     private IPlayerCommand Right;
     private IPlayerCommand Left;
     private IPlayerCommand Accelerate;
+
+    public GameObject Energy;
+    public Text EnergyText;
     // Start is called before the first frame update
     void Start()
     {
@@ -24,8 +28,13 @@ public class PlayerController : MonoBehaviour
         // Accelerate button to actually start moving in direction you are facing.
         if (Input.GetButton("Fire3"))
         {
-            if (Input.GetButton("Jump"))
+            var residualEnergy = this.Energy.GetComponent<Image>().fillAmount;
+            if (Input.GetButton("Jump") && residualEnergy != 0)
             {
+                residualEnergy -= 0.01f;
+                var value = (residualEnergy * 100);
+                this.Energy.GetComponent<Image>().fillAmount = residualEnergy > 0 ? residualEnergy : 0;
+                this.EnergyText.text = (Mathf.Ceil(residualEnergy * 100)).ToString();
                 this.Accelerate.Execute(this.gameObject);
             }
             else
