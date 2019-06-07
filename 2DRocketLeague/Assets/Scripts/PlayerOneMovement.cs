32b9ccb6695bf619eefcc37c080c0420d3a478db
FridaWang <379788704@qq.com>
2019-06-07 09:53:06 -0700

    lerp player's rotation when make movement

--- 2DRocketLeague/Assets/Scripts/PlayerOneMovement.cs
+++ 2DRocketLeague/Assets/Scripts/PlayerOneMovement.cs
@@ -4,8 +4,11 @@ namespace Player.Command
 {
     public class PlayerOneMovement : ScriptableObject, IPlayerCommand
     {
+        private float TurnSpeed = 180.0f;
+
         public void Execute(GameObject gameObject)
         {
+
             // Quick and dirty way to access the MovementSpeed variable from PlayerController.
             GameObject player = GameObject.Find ("player1");
             var movementSpeed = player.GetComponent<PlayerController>().MovementSpeed;
@@ -14,20 +17,35 @@ namespace Player.Command
             {
                 gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, movementSpeed, 0),  ForceMode.Force);
 
+                LerpPlayerRotation(gameObject, 90);
             }
             if (Input.GetKey(KeyCode.RightArrow))   // Right for Player 1
             {
                 gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(movementSpeed, 0, 0), ForceMode.Force);
 
+                LerpPlayerRotation(gameObject, 0);
             }
             if (Input.GetKey(KeyCode.DownArrow))  // down for Player 1
             {
                 gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, -movementSpeed, 0), ForceMode.Force);
 
+                LerpPlayerRotation(gameObject, -90);
             }
             if (Input.GetKey(KeyCode.LeftArrow))  // LEFt for Player 1
             {
                 gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-movementSpeed, 0, 0),  ForceMode.Force);
+
+                LerpPlayerRotation(gameObject, 180);
+            }
+        }
+
+        void LerpPlayerRotation(GameObject gameObject, float angle)
+        {
+            var rigidBody = gameObject.GetComponent<Rigidbody>();
+            var targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+            if (rigidBody != null)
+            {
+                rigidBody.transform.rotation = Quaternion.RotateTowards(rigidBody.transform.rotation, targetRotation, TurnSpeed * Time.deltaTime);
             }
         }
     }
--- 2DRocketLeague/Assets/Scripts/PlayerTwoMovement.cs
+++ 2DRocketLeague/Assets/Scripts/PlayerTwoMovement.cs
@@ -4,6 +4,8 @@ namespace Player.Command
 {
     public class PlayerTwoMovement : ScriptableObject, IPlayerCommand
     {
+        private float TurnSpeed = 180.0f;
+
         public void Execute(GameObject gameObject)
         {
             // Quick and dirty way to access the MovementSpeed variable from PlayerController.
@@ -13,21 +15,32 @@ namespace Player.Command
             if (Input.GetKey(KeyCode.W))  // UP for Player 2
             {
                 gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0,movementSpeed, 0),  ForceMode.Force);
+                LerpPlayerRotation(gameObject, 90);
             }
             if (Input.GetKey(KeyCode.D))   // Right for Player 2
             {
                 gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(movementSpeed, 0, 0),  ForceMode.Force);
-
+                LerpPlayerRotation(gameObject, 0);
             }
             if (Input.GetKey(KeyCode.S))  // down for Player 2
             {
                 gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0, -movementSpeed, 0),  ForceMode.Force);
-
+                LerpPlayerRotation(gameObject, -90);
             }
             if (Input.GetKey(KeyCode.A))  // LEFt for Player 2
             {
                 gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(-movementSpeed, 0, 0),  ForceMode.Force);
+                LerpPlayerRotation(gameObject, 180);
+            }
+        }
 
+        void LerpPlayerRotation(GameObject gameObject, float angle)
+        {
+            var rigidBody = gameObject.GetComponent<Rigidbody>();
+            var targetRotation = Quaternion.Euler(new Vector3(0f, 0f, angle));
+            if (rigidBody != null)
+            {
+                rigidBody.transform.rotation = Quaternion.RotateTowards(rigidBody.transform.rotation, targetRotation, TurnSpeed * Time.deltaTime);
             }
         }
     }
