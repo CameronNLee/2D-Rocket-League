6228a4c0b324c1de1864d2fbca813c42389d5888
FridaWang <379788704@qq.com>
2019-06-06 22:21:48 -0700

    add new assets / implement EnergySpawner.cs

---
+++ 2DRocketLeague/Assets/Scripts/EnergySpawner.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnergySpawner : MonoBehaviour
+{
+    public GameObject PrefabSmall;
+    public GameObject PrefabMedium;
+    public GameObject PrefabLarge;
+
+    private int MAXNUM = 6;
+    public static int count = 0;
+    private float elapsedTime = 0;
+
+    void Update()
+    {
+        elapsedTime += Time.deltaTime;
+        
+        if(elapsedTime > 5 && count < MAXNUM)
+        {
+
+            float x = Random.Range(-21,22);
+            float y = Random.Range(-13,14);
+            Vector3 position = new Vector3(x, y, -1);
+            var choice = Random.Range(1, 101);
+            if(choice <= 50)
+            {
+                Instantiate(PrefabSmall, position, Quaternion.identity);
+            }
+            else if(choice > 80)
+            {
+                Instantiate(PrefabLarge, position, Quaternion.identity);
+            }
+            else
+            {
+                Instantiate(PrefabMedium, position, Quaternion.identity);
+
+            }
+           
+            elapsedTime = 0f;
+            count += 1;
+        } 
+        
+    }
+}
