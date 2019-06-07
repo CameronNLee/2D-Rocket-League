0b848b7de05e0c36013fd160e956d43f3c4b9ee9
FridaWang <379788704@qq.com>
2019-06-06 18:24:12 -0700

    implement score system / implement scrolling text animation when score a goal/ implement the interaction between ball and gate / make changes to some objects' parameter to improve game feel

---
+++ 2DRocketLeague/Assets/Scripts/GateTrigger.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GateTrigger : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject newBall;
+    public Text teamScore;
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "ball")
+        {
+            ScrollTextManager.Instance.CreateText(player.transform.position, "+1", Color.yellow, 20);
+
+            // Destory the current ball
+            Destroy(collision.gameObject);
+
+            // Update team score
+            int score = int.Parse(teamScore.text);
+            score++;
+            teamScore.text = score.ToString();
+
+            // Instantiate a new ball
+            Vector3 startPoint = new Vector3(0, 0, -1);
+            Instantiate(newBall, startPoint, Quaternion.identity);
+        }
+    }
+}
---
+++ 2DRocketLeague/Assets/Scripts/ScrollTextManager.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScrollTextManager : MonoBehaviour
+{
+    private static ScrollTextManager instance;
+
+    public GameObject textPrefab;
+
+    public RectTransform canvasTransform;
+
+    public float speed;
+
+    public float fadeTime;
+
+    public Vector3 direction;
+
+    public static ScrollTextManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = GameObject.FindObjectOfType<ScrollTextManager>();
+            }
+            return instance;
+        }
+    }
+
+    public void CreateText(Vector3 position, string text, Color color, int fontsize)
+    {
+        GameObject sct = (GameObject)Instantiate(textPrefab, position, Quaternion.identity);
+        sct.transform.SetParent(canvasTransform);
+        sct.GetComponent<RectTransform>().localScale = new Vector3(1, 1, 1);
+        sct.GetComponent<ScrollText>().Initialize(speed, direction, fadeTime);
+        sct.GetComponent<Text>().text = text;
+        sct.GetComponent<Text>().color = color;
+        sct.GetComponent<Text>().fontSize = fontsize;
+    }
+}
